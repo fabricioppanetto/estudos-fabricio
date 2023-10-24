46c43b3819898e1df1181621a08a7a00ee870d4c
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2023-10-24 19:30:50 -0300

    JavaScript
    Aula + exercícios de variáveis

---
+++ estudos-front-end/javascript-completo/variaveis/script.js
@@ -0,0 +1,22 @@
+var nome = "Fabrício";
+var idade = 20;
+var possuiFaculdade = true
+
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var
+    sobrenome = "Panetto",
+    cidade = "Castelo";
+
+console.log(sobrenome, cidade)
+
+var semDefinir;
+
+console.log(semDefinir)
