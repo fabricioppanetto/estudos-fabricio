2d9d6e9d6bb88328ce90c37a55c32309836b8086
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2023-10-26 19:35:38 -0300

    JavaScript
    Aula de boolean e condicionais

---
+++ estudos-front-end/javascript-completo/boolean-e-condicionais/aula.js
@@ -0,0 +1,34 @@
+var possuiGraduacao = false;
+
+if(possuiGraduacao) {
+    console.log("É verdadeiro");
+} else {
+    console.log("É falso");
+}
+
+var nome = "";
+if(nome) {
+    console.log(nome);
+} else {
+    console.log("Não existe nome");
+}
+
+// Valores que retornam em false
+// if(false);
+// if(NaN);
+// if(0);
+// if(null);
+// if(undefined);
+// if("");
+
+// Valores que retornam em true
+// if(true);
+// if(1);
+// if(" ");
+// if(fabricio);
+// if(-5);
+// if({});
+
+if(!possuiGraduacao) {
+    console.log("Não possui graduação");
+}
