6fd289b8b159fe9e6f63b0570110bd08400e16cc
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2023-11-03 08:12:33 -0300

    JavaScript
    Aula de funções

--- estudos-front-end/javascript-completo/funcoes/aula.js
+++ estudos-front-end/javascript-completo/funcoes/aula.js
@@ -43,3 +43,43 @@ function mostraConsole() {
 addEventListener("click", mostraConsole);
 
 //ou addEventListener("click", function(){console.log("Oi")});
+//==================================================================
+
+function terceiraIdade(idade) {
+    console.log(typeof idade);
+    if(typeof idade !== "number") {
+        return "Por favor preencha um número"
+    } else if(idade >= 60) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(terceiraIdade("60"))
+
+//==================================================================
+
+function faltaVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Falta visitar ${totalPaises - paisesVisitados} países`;
+}
+
+console.log(faltaVisitar(60));
+
+//==================================================================
+
+var profissao = 'Analista de Redes';
+
+function dados() {
+    var nome = 'Fabrício'
+    var idade = 19;
+    function outrosDados() {
+        var endereco = 'Castelo';
+        var idade = 20;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}`
+    }
+    return outrosDados();
+}
+
+console.log(dados())
