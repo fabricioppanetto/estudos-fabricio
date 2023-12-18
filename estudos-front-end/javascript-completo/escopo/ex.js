56899f0cfe8c03812fb1c3fb520cd10bf9b07177
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2023-12-17 22:04:47 -0300

    JavaScript
    Aula + exercicios de Escopo, sobre Var, Const e Let.

---
+++ estudos-front-end/javascript-completo/escopo/ex.js
@@ -0,0 +1,29 @@
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+    console.log(cor, marca, portas);
+}
+
+//=========================================================
+
+const dois = 2;
+function somarDois(x) {
+    return x + dois;
+}
+function dividirDois(x) {
+    return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+//=========================================================
+
+const numero = 50;
+
+for(let numero = 0; numero <= 10; numero++) {
+    console.log(numero)
+}
+
+const total = 10 * numero;
+console.log(total);
