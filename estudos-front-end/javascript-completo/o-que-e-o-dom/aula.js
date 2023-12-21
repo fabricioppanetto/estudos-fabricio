ceefaf6f3fd0e8a22b0e9ee500a742d8b2cd4eae
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2023-12-20 21:17:54 -0300

    JavaScript
    Aula + exercicios sobre DOM

--- estudos-front-end/javascript-completo/o-que-e-o-dom/aula.js
+++ estudos-front-end/javascript-completo/o-que-e-o-dom/aula.js
@@ -1,3 +1,11 @@
 // window.alert('Maria, te amo S2')
 
 const h1Selecionado = document.querySelector('h1');
+
+const h1Classes = h1Selecionado.classList;
+
+function callbackh1(){
+    console.log('Clicou em ', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1);
---
+++ estudos-front-end/javascript-completo/o-que-e-o-dom/ex.js
@@ -0,0 +1,11 @@
+const href = window.location.href;
+console.log(href);
+
+const primeiroElemento = document.querySelector('.nome');
+console.log(primeiroElemento);
+
+const navegador = navigator.language;
+console.log(navegador);
+
+const largura = innerWidth
+console.log(largura);
