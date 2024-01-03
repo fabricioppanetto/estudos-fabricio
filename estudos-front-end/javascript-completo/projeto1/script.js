43070b714d13456270a338c3c52147f9e27d8809
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2024-01-02 21:28:32 -0300

    JavaScript
    Projeto Animais, aula sobre seleção de elementos.

--- estudos-front-end/javascript-completo/projeto1/script.js
+++ estudos-front-end/javascript-completo/projeto1/script.js
@@ -9,8 +9,25 @@ console.log(gridSection, contato);
 
 // Seleciona todas UL's, retorna uma HTMLCOllection
 const ul = document.getElementsByTagName('ul');
-
 console.log(ul);
 
 // Retorna o primeiro alemento
-console.log(gridSection[0])
+console.log(gridSection[0]);
+
+// Retorna a primeira li do site
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+// Retorna a primeira ul do site
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+// Retorna o primeiro link interno do site
+const linkInternos = document.querySelector('[href^="#"]');
+console.log(linkInternos);
+
+// Retorna todas as img que esta dentro da classe .animais
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[1]);
+
+
