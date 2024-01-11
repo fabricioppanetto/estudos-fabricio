7f5bc0eadf0ac4be4da18d78830eb54a6c0fee77
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2024-01-10 22:56:28 -0300

    JavaScript
    Aula + exercícios sobre classes e atributos

--- estudos-front-end/javascript-completo/projeto1/classes-e-atributos-aula.js
+++ estudos-front-end/javascript-completo/projeto1/classes-e-atributos-aula.js
@@ -1,2 +1,29 @@
 const menu = document.querySelector('.menu');
-console.log(menu.classList);
+
+menu.classList.add('ativo', 'teste');
+menu.classList.remove('azul');
+menu.classList.toggle('azul');
+// Se tem uma class AZUL, ele remove, se nao tem, ele adiciona uma classe AZUL
+
+if(menu.classList.contains('azul')) {
+    menu.classList.add('possuiAzul');
+} else {
+    menu.classList.add('naoPossuiAzul');
+}
+
+menu.classList += ' vermelho ';
+
+console.log(menu.className);
+
+// console.log(menu.classList);
+
+const animais = document.querySelector('.animais');
+console.log(animais.attributes['data-texto']);
+
+const img = document.querySelector('img');
+
+img.setAttribute('alt', "it's a fox");
+const possuiAlt = img.hasAttribute('alt');
+console.log(possuiAlt);
+
+console.log(img.getAttribute('src'));
