abec39f9fab714a2b15930da234e5feb676bc605
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2024-03-18 14:54:26 -0300

    JavaScript
    Exercícios sobre Constructor Function

--- estudos-front-end/javascript-completo/objetos/constructor-function/ex.js
+++ estudos-front-end/javascript-completo/objetos/constructor-function/ex.js
@@ -32,3 +32,24 @@ const bruno = new Pessoa('Bruno', 15);
 // elements, retorna NodeList com os elementos selecionados
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
+
+function Dom(seletor) {
+    const elementList = document.querySelectorAll(seletor);
+    this.elements = elementList;
+    this.addClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.add(classe);
+        })
+    }
+    this.removeClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.remove(classe);
+        })
+    }
+}
+
+const listaItens = new Dom('li');
+const ul = new Dom('ul');
+
+listaItens.addClass('ativar');
+ul.addClass('ativar-ul');
