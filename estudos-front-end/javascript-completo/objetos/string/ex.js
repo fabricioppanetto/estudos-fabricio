94ca712fa1b1685badfa46f4eca80678d3513fdc
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2024-04-26 13:38:07 -0300

    JavaScript
    String

--- estudos-front-end/javascript-completo/objetos/string/ex.js
+++ estudos-front-end/javascript-completo/objetos/string/ex.js
@@ -51,12 +51,30 @@ let html = `<ul>
                   <li><span>Contato</span></li>
                 </ul>`;
 
+html = html.split('span').join('a');
+
+console.log(html);
 
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
 
+console.log(frase[frase.length -1]);
+console.log(frase.slice(-1));
 
 
 // Retorne o total de taxas
 const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
 
+let taxasTotal = 0;
+transacoes2.forEach(function(item) {
+    item = item.toLowerCase();
+    item = item.trim();
+    item = item.slice(0,4);
+
+    if(item === 'taxa') {
+        taxasTotal += 1
+    }
+    console.log(item);
+});
+
+console.log(taxasTotal);
