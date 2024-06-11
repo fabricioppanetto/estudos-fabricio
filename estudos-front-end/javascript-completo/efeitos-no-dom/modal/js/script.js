92173d053b39ace3df4fce5c5c188204d00360b2
Fabrício Pimenta Panetto <118379897+fabricioppanetto@users.noreply.github.com>
2024-06-11 20:04:01 -0300

    JavaScript
    Modal

---
+++ estudos-front-end/javascript-completo/efeitos-no-dom/modal/js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initTabNav from './modules/nav-tab.js';
+import initAccordion from './modules/accordion.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
